b4c6ef0954fc19106dff6f79f3e5f4833ca7e74d
Meoarch <157293352+QwQdoge@users.noreply.github.com>
2026-06-29 10:18:57 +0800

    Update generated plugin registrants

--- FlutterUI/macos/Flutter/GeneratedPluginRegistrant.swift
+++ FlutterUI/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,25 +6,33 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import device_info_plus
 import file_picker
 import flutter_local_notifications
 import flutter_secure_storage_darwin
+import package_info_plus
+import passkeys_darwin
 import screen_retriever_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import system_tray
+import ua_client_hints
 import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PasskeysPlugin.register(with: registry.registrar(forPlugin: "PasskeysPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
+  UAClientHintsPlugin.register(with: registry.registrar(forPlugin: "UAClientHintsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- FlutterUI/windows/flutter/generated_plugins.cmake
+++ FlutterUI/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   flutter_secure_storage_windows
+  passkeys_windows
   screen_retriever_windows
   system_tray
   url_launcher_windows
